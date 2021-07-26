03e633defe544e95a75fbc77861ef9014aebb438
sarowar-al-amin <sajib.sarowaralamin123@gmail.com>
2021-07-26 11:23:33 +0600

    Create student create, edit, list and detail pages And modify app.routing.ts as requirement

---
+++ IonicHttpClient/src/app/student-create/student-create.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-student-create',
+  templateUrl: './student-create.page.html',
+  styleUrls: ['./student-create.page.scss'],
+})
+export class StudentCreatePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
